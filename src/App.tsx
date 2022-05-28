73e0808d01dd665ddd17d5ca3bd8118df1aa93cc
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-05-28 21:46:07 +0430

    install uuid for generate random id

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,43 @@ import React, { useState } from "react";
 import "./App.css";
 import NavbarInput from "./components/Navbar/NavbarInput";
 import Todos from "./components/Todos/Todos";
- export interface Todo {
-  id:number | string;
-  todo:string;
-  completed:boolean;
+import { v4 as uuid } from "uuid";
+export interface Todo {
+  id: number | string;
+  todo: string;
+  completed: boolean;
 }
 
 const App = () => {
-
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodo =(e:React.FormEvent) =>{
+  const addTodo = (e: React.FormEvent) => {
     e.preventDefault();
-    if(todo){
-      setTodos(prevState => [...prevState,{todo,id:Date.now(),completed:false}]);
+    if (todo) {
+      setTodos((prevState) => [
+        ...prevState,
+        { todo, id: uuid(), completed: false },
+      ]);
       setTodo("");
     }
-  }
+  };
 
-  const deleteTodo = (id:number | string ) =>{
+  
+  const deleteTodo = (id: number | string) => {
     const allTodos = [...todos];
-    const filtredTodo = allTodos.filter(todo => todo.id !== id );
+    const filtredTodo = allTodos.filter((todo) => todo.id !== id);
     setTodos(filtredTodo);
-
-  }
+  };
 
   // console.log(todos);
 
   return (
     <div className="App">
       <h1>saman keighobadi</h1>
-      <NavbarInput  todo={todo} setTodo={setTodo} addTodo={addTodo} />
+      <NavbarInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
       <Todos todos={todos} deleteTodo={deleteTodo} />
     </div>
-  
   );
 };
 
